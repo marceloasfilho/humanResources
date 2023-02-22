83a5eea87d71611bd250b677dc288089b29ae990
marceloasfilho <marceloandradesilvafilho@gmail.com>
2023-02-22 11:16:22 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/github/marceloasfilho/humanresources/HumanResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marceloasfilho.humanresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
